f7a9be020bd206ac2c495e7203afad193f6ca1d7
Emily Znamierowski <emilyannz@gmail.com>
2016-04-23 12:32:17 -0500

    Updated application route

--- app/application/route.js
+++ app/application/route.js
@@ -1,3 +1,46 @@
+
+import toggleNav from 'toggle-login';
+
+export default class FormView {
+  constructor(form, app) {
+    this.form = document.querySelector(`form.login-form`);
+    this.app = app;
+
+    toggleNav();
+
+    this.form.addEventListener(`submit`, (ev) => {
+      ev.preventDefault();
+
+      const getValues = {
+        userName: this.form.querySelector(`[userName=userName]`).value,
+        email: this.form.querySelector(`[email=email]`).value,
+      };
+
+      // fetch(`We will get this URL from the backend team, {
+        method: `POST`,
+        headers: {
+          Accept: `application/json`,
+          'Content-Type': `application/json`,
+        },
+        body: JSON.stringify(getValues),
+      }).then((res) => res.json())
+      .then((data) => {
+        this.form.reset();
+
+
+// We might explore adding this after we complete the basic functionality
+        this.app.add(data);
+        document.querySelector(`.main`).classList.toggle(`slide`);
+      });
+    });
+  }
+}
+
+
+
+
+
+// OTHER OPTION
 // import Ember from 'ember';
 //
 // export default Ember.Route.extend({
@@ -15,43 +58,3 @@
 // });
 //
 // 'use strict';
-import toggleNav from 'toggle-nav';
-
-export default class FormView {
-  constructor(form, app) {
-    this.form = document.querySelector(`form.nav-form`);
-    this.app = app;
-
-    toggleNav();
-
-    this.form.addEventListener(`submit`, (ev) => {
-      ev.preventDefault();
-
-      const getValues = {
-        name: this.form.querySelector(`[name=name]`).value,
-        age: this.form.querySelector(`[name=age]`).value,
-        photoUrl: this.form.querySelector(`[name=photo-url]`).value,
-        profile: this.form.querySelector(`[name=profile]`).value,
-      };
-
-      fetch(`http://tiny-tn.herokuapp.com/collections/nt-puppy`, {
-        method: `POST`,
-        headers: {
-          Accept: `application/json`,
-          'Content-Type': `application/json`,
-        },
-        body: JSON.stringify(getValues),
-      }).then((res) => res.json())
-      .then((data) => {
-        this.form.reset();
-        // this.form.querySelector(`[name=name]`).value = ``;
-        // this.form.querySelector(`[name=age]`).value = ``;
-        // this.form.querySelector(`[name=photo-url]`).value = ``;
-        // this.form.querySelector(`[name=profile]`).value = ``;
-
-        this.app.add(data);
-        document.querySelector(`.main`).classList.toggle(`slide`);
-      });
-    });
-  }
-}
