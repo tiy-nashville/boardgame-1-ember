0578ec621d380108dcb0a4bc63393458c70e53fd
Emily Znamierowski <emilyannz@gmail.com>
2016-04-23 08:05:34 -0500

    Created login route but broccoli files need to be fixed

---
+++ app/login/controller.js
@@ -0,0 +1,13 @@
+{/*<form class="login-form" {{action login model on="submit"}}>
+  <p class="form-field">
+    <label>Email</label>
+    {{input value=model.email}}
+  </p>
+
+  <p class="form-field">
+    <label>Password</label>
+    {{input type="password" value=model.password}}
+  </p>
+
+  <button>Login</button>
+</form>*/}
