748ec78ef2ec29df6274e72204d3ef11f83527e3
Emily Znamierowski <emilyannz@gmail.com>
2016-04-23 17:16:29 -0500

    Cleaning up merged code

--- app/index/route.js
+++ app/index/route.js
@@ -1,11 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-<<<<<<< HEAD
   model() {
     return fetch
   }
-=======
-
->>>>>>> 38e419a2b1b3b07e58492b8c2d149b410d376870
 });
--- app/router.js
+++ app/router.js
@@ -6,14 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-<<<<<<< HEAD
-  this.route('scrabble');
-  this.route('life');
   this.route('cards-against-humanity');
   this.route('clue');
   this.route('login');
   this.route('form');
-=======
   this.route('sorry');
   this.route('monopoly');
   this.route('life');
@@ -22,7 +18,6 @@ Router.map(function() {
   this.route('cah');
   this.route('add-game');
   this.route('checkout');
->>>>>>> 38e419a2b1b3b07e58492b8c2d149b410d376870
 });
 
 export default Router;
