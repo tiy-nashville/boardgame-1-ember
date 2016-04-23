54791eea29e7a4616f0655f499e7cb41c334ebea
Emily Znamierowski <emilyannz@gmail.com>
2016-04-23 17:49:35 -0500

    Continue to clean up merged files

--- app/index/route.js
+++ app/index/route.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return fetch
-  }
+  model(url) {
+    return fetch(`http://8cb7cf5b.ngrok.io/add-game`)
+    .then(res => res.json());
+  },
 });
