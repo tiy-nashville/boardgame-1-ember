2c8d78f6bd59bd4e9feb7665d6c9c4f116756ee3
Emily Znamierowski <emilyannz@gmail.com>
2016-04-23 12:29:39 -0500

    Updated Login form; added handlebar code

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,4 +1,17 @@
-import Ember from 'ember';
+// 14-puppy-adoption/src/toggle-nav.js
 
-export default Ember.Controller.extend({
-});
+// import Ember from 'ember';
+//
+// export default Ember.Controller.extend({
+// });
+//
+//
+'use strict';
+
+export default function toggleSlider() {
+  const toggleButton = document.querySelector(`.top-nav__icon`);
+  const main = document.querySelector(`.main`);
+  toggleButton.addEventListener(`click`, () => {
+    main.classList.toggle(`slide`);
+  });
+}
